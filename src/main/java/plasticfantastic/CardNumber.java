425701ca6e7ec79f6c2b3c7b036fdfe7e79c7247
Chris Vaughan <chris.vaughan@laterooms.com>
2015-07-07 12:59:24 +0100

    Fixed random non-UTF8 character

--- src/main/java/plasticfantastic/CardNumber.java
+++ src/main/java/plasticfantastic/CardNumber.java
@@ -67,7 +67,7 @@ public final class CardNumber {
     public boolean passesLuhnCheck() {
         /* Description from Wikipedia:
          * 1. From the rightmost digit, which is the check digit, moving left, double the value of every second digit;
-         * if the product of this doubling operation is greater than 9 (e.g., 8 × 2 = 16), then sum the digits of the
+         * if the product of this doubling operation is greater than 9 (e.g., 8 * 2 = 16), then sum the digits of the
          * products (e.g., 16: 1 + 6 = 7, 18: 1 + 8 = 9).
          * 2. Take the sum of all the digits.
          * 3. If the total modulo 10 is equal to 0 (if the total ends in zero) then the number is valid according to
